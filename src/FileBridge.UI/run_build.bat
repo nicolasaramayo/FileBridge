96be5c1da76336a920fe7cafce76009d23dd023d
nicolasaramayo <nicolasaramayo@users.noreply.github.com>
2026-03-22 15:02:59 -0300

    feat: Add common script file extensions to `.gitignore`.

--- src/FileBridge.UI/run_build.bat
+++
@@ -1,3 +0,0 @@
-@echo off
-dotnet build FileBridge.UI.csproj > build_output.log 2>&1
-echo Done!
--- src/FileBridge.UI/run_build.ps1
+++
@@ -1,2 +0,0 @@
-$proc = Start-Process dotnet -ArgumentList "build" -WorkingDirectory "c:\Projects\FileBridge\src\FileBridge.UI" -RedirectStandardOutput "c:\Projects\FileBridge\src\FileBridge.UI\build_output.log" -RedirectStandardError "c:\Projects\FileBridge\src\FileBridge.UI\build_error.log" -PassThru -Wait
-echo "Done"
